4167de6661d400aa89e71aaacde0b66872cb101c
Ankur Raj <141816276+rajankurpt@users.noreply.github.com>
2026-02-06 21:27:58 +0530

    Update db.js

--- document-manager/backend/config/db.js
+++ document-manager/backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '', // your MySQL password
-  database: process.env.DB_NAME || 'dept_project',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
 });
