5e30d241f5853c5dd0207f9ad92daaa5020f92af
Ankur Raj <rajankurpt@gmail.com>
2026-02-06 22:54:09 +0530

    DB.js

--- document-manager/backend/config/db.js
+++ document-manager/backend/config/db.js
@@ -1,14 +1,7 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
+
 
 const initDb = async () => {
   try {
